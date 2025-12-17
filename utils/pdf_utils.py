ce000e016c1ce0e0cc624e4edf8ffb7daee1bf03
Pranav Vikraman s s <pranav.vikraman.25@gmail.com>
2025-12-17 18:43:34 +0530

    Update pdf_utils.py

--- utils/pdf_utils.py
+++ utils/pdf_utils.py
@@ -1,7 +1,6 @@
 import fitz  # PyMuPDF
 
-def extract_pages_and_images(uploaded_file):
-    pdf_bytes = uploaded_file.read()
+def extract_pages_and_images(pdf_bytes):
     doc = fitz.open(stream=pdf_bytes, filetype="pdf")
 
     page_images = []
@@ -9,8 +8,8 @@ def extract_pages_and_images(uploaded_file):
 
     for page in doc:
         page_texts.append(page.get_text())
-
         pix = page.get_pixmap(dpi=150)
         page_images.append(pix.tobytes("png"))
 
     return page_images, page_texts
+
