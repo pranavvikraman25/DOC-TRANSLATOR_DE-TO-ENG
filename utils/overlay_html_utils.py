649abcfbb1e3841539549d60753f69fac57f4b7e
Pranav Vikraman s s <pranav.vikraman.25@gmail.com>
2025-12-17 18:33:35 +0530

    Update overlay_html_utils.py

--- utils/overlay_html_utils.py
+++ utils/overlay_html_utils.py
@@ -1,8 +1,7 @@
 def build_overlay_html(image_base64, ocr_boxes):
-    html_boxes = ""
-
+    overlays = ""
     for box in ocr_boxes:
-        html_boxes += f"""
+        overlays += f"""
         <div style="
             position:absolute;
             left:{box['x']}px;
@@ -10,24 +9,17 @@ def build_overlay_html(image_base64, ocr_boxes):
             width:{box['w']}px;
             height:{box['h']}px;
             background:rgba(255,255,0,0.35);
-            font-size:12px;
             cursor:pointer;
-        "
-        title="{box['translated']}"
-        ></div>
+        " title="{box['translated']}"></div>
         """
 
     return f"""
-    <div style="position:relative; display:inline-block;">
-        <img src="data:image/png;base64,{image_base64}"
-             style="width:100%;"/>
-
-        <div style="position:absolute; top:0; left:0;">
-            {html_boxes}
+    <div style="position:relative;">
+        <img src="data:image/png;base64,{image_base64}" style="width:100%;" />
+        <div style="position:absolute;top:0;left:0;">
+            {overlays}
         </div>
     </div>
-
-    <p style="font-size:14px; color:#555;">
-    🟨 Hover over highlighted areas to see English translation
-    </p>
+    <p>🟨 Hover highlighted areas for English translation</p>
     """
+
